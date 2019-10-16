067c1b4edcd95002d75cc9fb1c9441137f7cbd24
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:24:03 +0200

    finito

--- Ultimo/Program.cs
+++ Ultimo/Program.cs
@@ -6,7 +6,29 @@ namespace Ultimo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci il costo del primo prodotto:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci il costo del secondo prodotto");
+            double b = double.Parse(Console.ReadLine());
+            if (a < b)
+            {
+                double max = b;
+                double min = a;
+                min = min / 2;
+                double prezzo = min + max;
+                Console.WriteLine($"il costo finale è di {prezzo:c}");
+            }
+            else
+            {
+                double max = a;
+                double min = b;
+                min = min / 2;
+                double prezzo = min + max;
+                Console.WriteLine($"il costo finale è di {prezzo:c}");
+            }
+            Console.ReadLine();
         }
     }
 }
+
+        
